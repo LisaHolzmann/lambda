627296da5f6667657a4f943699f1a2458e5c909f
LisaHolzmann <holzmann.lisa@gmx.at>
2020-09-28 15:28:16 +0200

    Lambda PalindromeTester

--- src/hue2/Main.java
+++ src/hue2/Main.java
@@ -33,7 +33,21 @@ public class Main {
             }
             return true;
         });
-        nt.setPalindromeTester();
+        nt.setPalindromeTester((n) -> {
+            String text = Integer.toString(n);
+            String clean = text.replaceAll("\\s+", "").toLowerCase();
+            int length = clean.length();
+            int forward = 0;
+            int backward = length - 1;
+            while (backward > forward) {
+                char forwardChar = clean.charAt(forward++);
+                char backwardChar = clean.charAt(backward--);
+                if (forwardChar != backwardChar) {
+                    return false;
+                }
+            }
+            return true;
+        });
 
     }
 }
