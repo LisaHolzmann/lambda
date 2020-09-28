c0b7fb531135ac29b66422389b59f0e4aa5cc950
LisaHolzmann <holzmann.lisa@gmx.at>
2020-09-28 14:12:20 +0200

    4.Aenderung

--- src/hue2/NumberTester.java
+++ src/hue2/NumberTester.java
@@ -5,16 +5,45 @@
  */
 package hue2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author holzm
  */
+//3.Beispiel
 public class NumberTester {
 
     private String fileName;
+    private int testfaelle;
+    private Map<Integer, Integer> zahlen = new HashMap<>();
 
     public NumberTester(String fileName) {
         this.fileName = fileName;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+            String line = br.readLine();
+            this.testfaelle = Integer.parseInt(line);
+            line = br.readLine();
+            while (line != null) {
+                String[] splitted = line.split(" ");
+                zahlen.put(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[0]));
+
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
