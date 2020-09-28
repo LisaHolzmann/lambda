a725228e30a3845a4cf6079124fb3388da84f40c
LisaHolzmann <holzmann.lisa@gmx.at>
2020-09-28 13:28:26 +0200

    1.Aenderung

---
+++ src/hue2/HalloJacaMitForEach.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author holzm
+ */
+public class HalloJacaMitForEach {
+
+    public static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        list.add("Hallo");
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        list.forEach(System.out::print);
+    }
+}
