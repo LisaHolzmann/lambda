44ce5ef3efc6942c744d7c74b71c7da825d8f8f3
LisaHolzmann <holzmann.lisa@gmx.at>
2020-09-29 19:21:37 +0200

    Calculator Methoden implementiert

--- src/hue2/RationalCalculator.java
+++ src/hue2/RationalCalculator.java
@@ -9,6 +9,30 @@ package hue2;
  *
  * @author holzm
  */
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator {
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return super.add.calc(a, b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return super.subtract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return super.multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return super.divide.calc(a, b);
+    }
 
 }
