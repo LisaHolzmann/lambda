380badea2b44e3342d032292e6f5c9317c08def8
LisaHolzmann <holzmann.lisa@gmx.at>
2020-10-04 23:12:09 +0200

    Aenderungen main Methode

--- src/hue2/bsp4/Main2.java
+++ src/hue2/bsp4/Main2.java
@@ -18,6 +18,7 @@ public class Main2 {
 
         Scanner scanner = new Scanner(System.in, "Windows-1252");
 
+        //add
         RationalCalculator rc = new RationalCalculator((a, b) -> {
             double zaehler = a.getA() * b.getB() + b.getA() * a.getB();
             double nenner = a.getB() * b.getB();
@@ -25,6 +26,7 @@ public class Main2 {
             number.setA(zaehler);
             number.setB(nenner);
             return number;
+            //sub
         }, (a, b) -> {
             double zaehler = a.getA() * b.getB() - b.getA() * b.getB();
             double nenner = a.getB() * b.getA() + b.getB() * a.getA();
@@ -32,6 +34,7 @@ public class Main2 {
             number.setA(zaehler);
             number.setB(nenner);
             return number;
+            //mul
         }, (a, b) -> {
             double zaehler = a.getA() * b.getA();
             double nenner = a.getB() * b.getB();
@@ -39,6 +42,7 @@ public class Main2 {
             number.setA(zaehler);
             number.setB(nenner);
             return number;
+            //div
         }, (a, b) -> {
             double zaehler = a.getA() * b.getB();
             double nenner = a.getB() * b.getA();
@@ -48,6 +52,7 @@ public class Main2 {
             return number;
         });
 
+        //add
         VectorCalculator vc = new VectorCalculator((a, b) -> {
             double x = a.getA() + b.getA();
             double y = b.getB() + b.getB();
@@ -55,6 +60,7 @@ public class Main2 {
             number.setA(x);
             number.setB(y);
             return number;
+            //sub
         }, (a, b) -> {
             double x = a.getA() - b.getA();
             double y = a.getB() - b.getB();
@@ -62,12 +68,14 @@ public class Main2 {
             number.setA(x);
             number.setB(y);
             return number;
+            //mul
         }, (a, b) -> {
             double zaehler = a.getA() * b.getB() + a.getB() * b.getA();
             Number number = new Number();
             number.setA(zaehler);
             number.setB(1);
             return number;
+            //div
         }, (a, b) -> {
             double zaehler = a.getA() * b.getA() + a.getB() + b.getB();
             Number number = new Number();
@@ -75,7 +83,8 @@ public class Main2 {
             number.setB(1);
             return number;
         });
-
+        //a-reeller Teil, b-imaginaerer Teil
+        //add
         ComplexCalculator cc = new ComplexCalculator((a, b) -> {
             double x = a.getA() + b.getB();
             double y = a.getB() + b.getB();
@@ -83,6 +92,7 @@ public class Main2 {
             number.setA(x);
             number.setB(y);
             return number;
+            //sub
         }, (a, b) -> {
             double x = a.getA() - b.getB();
             double y = a.getB() - b.getB();
@@ -90,6 +100,7 @@ public class Main2 {
             number.setA(x);
             number.setB(y);
             return number;
+            //mul
         }, (a, b) -> {
             double x = a.getA() * b.getA() - a.getB() * b.getB();
             double y = a.getA() * b.getA() + a.getB() * b.getB();
@@ -97,9 +108,11 @@ public class Main2 {
             number.setA(x);
             number.setB(y);
             return number;
+            //div
         }, (a, b) -> {
-            double x = a.getA() + a.getB();
-            double y = b.getA() + b.getB();
+            double nenner = b.getA() * b.getA() + b.getB() + b.getB();
+            double x = (a.getA() * b.getA() + a.getB() * b.getB()) / nenner;
+            double y = (a.getB() * b.getA() - a.getA() * b.getB()) / nenner;
             Number number = new Number();
             number.setA(x);
             number.setB(y);
@@ -111,22 +124,31 @@ public class Main2 {
 
         while (ausw < 4) {
 
-            System.out.print("Nummer eigeben x a: \n");
-            int xa = Integer.parseInt(scanner.nextLine());
-            System.out.print("Nummer eingeben x b: \n");
-            int xb = Integer.parseInt(scanner.nextLine());
-            System.out.print("Nummer eingeben y a: \n");
-            int ya = Integer.parseInt(scanner.nextLine());
-            System.out.print("Nummer eingeben y b: \n");
-            int yb = Integer.parseInt(scanner.nextLine());
+            int operation;
+            int xa;
+            int xb;
+            int ya;
+            int yb;
+            do {
+                System.out.print("Nummer eigeben x a: \n");
+                xa = Integer.parseInt(scanner.nextLine());
+                System.out.print("Nummer eingeben x b: \n");
+                xb = Integer.parseInt(scanner.nextLine());
+                System.out.print("Nummer eingeben y a: \n");
+                ya = Integer.parseInt(scanner.nextLine());
+                System.out.print("Nummer eingeben y b: \n");
+                yb = Integer.parseInt(scanner.nextLine());
 
-            System.out.println("Choose operation:");
-            System.out.println("1.. add");
-            System.out.println("2.. subtract");
-            System.out.println("3.. multiply");
-            System.out.println("4.. divide");
-            ;
-            int operation = Integer.parseInt(scanner.nextLine());
+                System.out.println("Choose operation:");
+                System.out.println("1.. add");
+                System.out.println("2.. subtract");
+                System.out.println("3.. multiply");
+                System.out.println("4.. divide");
+                System.out.println("5.. enter numbers again");
+
+                operation = Integer.parseInt(scanner.nextLine());
+            } while (operation == 5);
+
             Number number1 = new Number();
             number1.setA(xa);
             number1.setB(xb);
@@ -134,8 +156,10 @@ public class Main2 {
             number2.setA(ya);
             number2.setB(yb);
             Number erg = new Number();
+
             switch (ausw) {
                 case 1:
+
                     switch (operation) {
                         case 1:
 
@@ -153,6 +177,26 @@ public class Main2 {
 
                             erg = rc.divide(number1, number2);
                             break;
+
+                        case 5:
+                            System.out.print("Nummer eigeben x a: \n");
+                            xa = Integer.parseInt(scanner.nextLine());
+                            System.out.print("Nummer eingeben x b: \n");
+                            xb = Integer.parseInt(scanner.nextLine());
+                            System.out.print("Nummer eingeben y a: \n");
+                            ya = Integer.parseInt(scanner.nextLine());
+                            System.out.print("Nummer eingeben y b: \n");
+                            yb = Integer.parseInt(scanner.nextLine());
+
+                            number1 = new Number();
+                            number1.setA(xa);
+                            number1.setB(xb);
+                            number2 = new Number();
+                            number2.setA(ya);
+                            number2.setB(yb);
+                            erg = new Number();
+                            break;
+
                     }
                     break;
 
@@ -214,9 +258,8 @@ public class Main2 {
             System.out.println("b : " + erg.getB());
             menu();
             ausw = Integer.parseInt(scanner.nextLine());
-        }
-        System.exit(0);
 
+        }
     }
 
     private static void menu() {
